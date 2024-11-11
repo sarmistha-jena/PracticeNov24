bce5ab087e441e7a2aade9a49ab2cd8492a8f249
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-11 19:54:25 +0530

    fix terra

--- terra/jenkins/master/main.tf
+++ terra/jenkins/master/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "server" {
-  provider      = aws.mumbai
-  ami           = "ami-08e5424edfe926b43"
-  instance_type = var.instance_type
-  security_groups = ["sg-08fbed9a4057dfe9a"]
+  provider                    = aws.mumbai
+  ami                         = "ami-08e5424edfe926b43"
+  instance_type               = var.instance_type
+  security_groups             = ["sg-08fbed9a4057dfe9a"]
   key_name                    = "linux-pract1"
   associate_public_ip_address = true
-  tags          = {
+  tags                        = {
     Name = var.server_name
   }
 }
