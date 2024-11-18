7a9e379d0641501a41dc4cf7a99e961029deafc7
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-18 18:13:39 +0530

    TF: added data source example

--- terra/utility-1/main.tf
+++ terra/utility-1/main.tf
@@ -1,6 +1,14 @@
+# Get List of Availability Zones in a Specific Region
+# Region is set in c1-versions.tf in Provider Block
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 # Datasource
 data "aws_ec2_instance_type_offerings" "my_ins_type1" {
-  for_each = toset(["ap-south-1a", "ap-south-1b", "ap-south-1c"])
+  for_each = toset(data.aws_availability_zones.my_azones.names)
   filter {
     name   = "instance-type"
     values = ["t3.micro"]
@@ -23,4 +31,29 @@ output "output_v2_1" {
 output "output_v2_2" {
   value = { for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
   az => details.instance_types }
+}
+
+# Basic Output: All Availability Zones mapped to Supported Instance Types
+output "output_v3_1" {
+  value = { for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
+  az => details.instance_types }
+}
+
+# Filtered Output: Exclude Unsupported Availability Zones
+output "output_v3_2" {
+  value = { for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
+  az => details.instance_types if length(details.instance_types) != 0 }
+}
+
+# Filtered Output: with Keys Function - Which gets keys from a Map
+# This will return the list of availability zones supported for a instance type
+output "output_v3_3" {
+  value = keys({ for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
+  az => details.instance_types if length(details.instance_types) != 0 })
+}
+
+# Filtered Output: As the output is list now, get the first item from list (just for learning)
+output "output_v3_4" {
+  value = keys({ for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
+  az => details.instance_types if length(details.instance_types) != 0 })[0]
 }
