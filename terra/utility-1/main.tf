c780225d6fbe4591d92dbd879e2f82218d55d1ea
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-18 18:05:05 +0530

    TF: added data source example

--- terra/utility-1/main.tf
+++ terra/utility-1/main.tf
@@ -1,12 +1,13 @@
 # Datasource
 data "aws_ec2_instance_type_offerings" "my_ins_type1" {
+  for_each = toset(["ap-south-1a", "ap-south-1b", "ap-south-1c"])
   filter {
     name   = "instance-type"
     values = ["t3.micro"]
   }
   filter {
     name   = "location"
-    values = ["ap-south-1a"]
+    values = [each.key]
     #values = ["us-east-1e"]
   }
   location_type = "availability-zone"
@@ -14,6 +15,12 @@ data "aws_ec2_instance_type_offerings" "my_ins_type1" {
 
 
 # Output
-output "output_v1_1" {
-  value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+output "output_v2_1" {
+  #value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+  value = toset([ for t in data.aws_ec2_instance_type_offerings.my_ins_type1 : t.instance_types])
+}
+
+output "output_v2_2" {
+  value = { for az, details in data.aws_ec2_instance_type_offerings.my_ins_type1 :
+  az => details.instance_types }
 }
