1ab446e219a202a446c9c6edf55864d96e739922
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-18 19:13:21 +0530

    TF: added check examples

--- terra/checkCond/main.tf
+++ terra/checkCond/main.tf
@@ -16,7 +16,7 @@ check "health_check" {
   }*/
 
   assert {
-    condition     = data.http.terraform_io.status_code == 200
+    condition     = data.http.terraform_io.status_code == 201
     error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
   }
 }
