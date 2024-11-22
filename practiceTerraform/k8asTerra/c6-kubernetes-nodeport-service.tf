40229fa66ee81757bb1194d9547b130d33cf0fb2
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-22 19:12:43 +0530

    TF: added k8 manifest as terra resources

--- practiceTerraform/k8asTerra/c6-kubernetes-nodeport-service.tf
+++ practiceTerraform/k8asTerra/c6-kubernetes-nodeport-service.tf
@@ -1,9 +1,10 @@
-resource "kubernetes_secret_v1" "np_service" {
+# Kubernetes Service Manifest (Type: Node Port Service)
+resource "kubernetes_service_v1" "np_service" {
   metadata {
     name = "myapp1-nodeport-service"
   }
   spec {
-    selector= {
+    selector = {
       app = kubernetes_deployment_v1.myapp1.spec.0.selector.0.match_labels.app
     }
     port {
