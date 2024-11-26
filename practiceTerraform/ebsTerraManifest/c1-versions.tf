6afe8769f262f87ca9f8869c0e405a6860772222
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-26 16:59:04 +0530

    TF: added EBS CSI configs

---
+++ practiceTerraform/ebsTerraManifest/c1-versions.tf
@@ -0,0 +1,36 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = "~> 3.70"
+      version = ">= 4.65"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      #version = "2.4.1"
+      #version = "~> 2.4"
+      version = "~> 2.9"
+    }
+    http = {
+      source = "hashicorp/http"
+      #version = "2.1.0"
+      #version = "~> 2.1"
+      version = "~> 3.3"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "my-tf-test-bucket-backend"
+    key    = "k8asTerra/terraform.tfstate" # path & file which will hold the state #
+    region = "ap-south-1"
+    #dynamodb_table = "terraform-lock"
+    #encrypt        = "true"
+    # For State Locking
+    #dynamodb_table = "dev-ekscluster"
+  }
+}
+provider "http" {
+  # Configuration options
+}
---
+++ practiceTerraform/ebsTerraManifest/c2-remote-state-datasource.tf
@@ -0,0 +1,19 @@
+# Terraform Remote State Datasource
+/*
+data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../eksOnlySetUp/terraform.tfstate"
+  }
+}
+*/
+
+# Terraform Remote State Datasource - Remote Backend AWS S3
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+  config = {
+    bucket = "my-tf-test-bucket-backend"
+    key    = "ebsTerraManifest/terraform.tfstate" # path & file which will hold the state #
+    region = "ap-south-1"
+  }
+}
