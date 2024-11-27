18c3fc7058196b2ce5bcf528c92f15e2e5c49c57
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-27 16:40:33 +0530

    TF: added app manifests in tf format - resize ebs

--- practiceTerraform/umsTerraK8/c4-02-persistent-volume-claim.tf
+++ practiceTerraform/umsTerraK8/c4-02-persistent-volume-claim.tf
@@ -8,7 +8,7 @@ resource "kubernetes_persistent_volume_claim_v1" "pvc" {
     storage_class_name = kubernetes_storage_class_v1.ebs_sc.metadata.0.name
     resources {
       requests = {
-        storage = "4Gi"
+        storage = "6Gi"
       }
     }
   }
