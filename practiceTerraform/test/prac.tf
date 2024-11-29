93c4ccfdfb025ddc4b756266bcc41570f4cba84a
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-29 13:23:06 +0530

    TF: added iam user tf config

--- practiceTerraform/test/prac.tf
+++ practiceTerraform/test/prac.tf
@@ -2,18 +2,18 @@ provider "terraform" {
   required_version = ">= 1.0.0"
 }
 variable "food" {
-  type = string
+  type    = string
   default = "carrot"
   validation {
     //condition = contains(["carrot", "apple"], var.food)
-    condition = can(regex("^[a-z]+$", var.food))
+    condition     = can(regex("^[a-z]+$", var.food))
     error_message = "Variable contains numbers"
   }
 }
-resource "null_resource" "cond" {
+locals {
   typeF = var.food == "carrot" ? "veggie" : "fruit"
 }
 
 output "foodType" {
-  value = null_resource.cond.typeF
+  value = local.typeF
 }
