9c4acdabb6f09e7f936b20e637f5f66435adc4f8
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-11-29 18:40:14 +0530

    TF: added iam group tf config

---
+++ practiceTerraform/roleIAMWithSetUp/c2-version.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.31"
+    }
+  }
+
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "my-tf-test-bucket-backend"
+    key    = "eksOnlySetUp/terraform.tfstate" # path & file which will hold the state #
+    region = "ap-south-1"
+    #dynamodb_table = "terraform-lock"
+    #encrypt        = "true"
+    # For State Locking
+    #dynamodb_table = "dev-ekscluster"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ practiceTerraform/roleIAMWithSetUp/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region        = "ap-south-1"
+environment       = "test"
+business_division = "ops"
